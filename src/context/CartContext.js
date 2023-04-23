6054230d7efb1452f8e7384da71115afb7c38140
Cristian Figueroa <cj.figueroa.rodriguez@gmail.com>
2023-04-23 15:13:13 -0300

    implementacion inicial del carrito

---
+++ src/context/CartContext.js
@@ -0,0 +1,54 @@
+import { useState, createContext, useContext } from 'react'
+
+const CartContext = createContext()
+
+export const useCartContext = () => useContext(CartContext)
+
+export const CartProvider = (props) => {
+
+    const [cart, setCart] = useState([])
+
+    //buscar producto
+
+    const isInCart = (queryID) => {
+        return cart.some(item => item.id === queryID)
+    }
+
+    const addItem = (item, amount) => {
+        if(isInCart(item.id)) {
+            const newCart = [...cart]
+            newCart[cart.findIndex(queryItem => queryItem.id === item.id)].amount = amount
+
+            setCart(newCart)
+        } else {
+            const newItem = {
+                ...item,
+                amount
+            }
+
+            setCart([...cart, newItem])
+        }
+    }
+
+    const removeItem = (itemID) => {
+        setCart(cart.filter(filterItem => filterItem.id !== itemID))
+    }
+
+    const emptyCart = () => setCart([])
+
+    const getItemQuantity = () => {
+        return cart.reduce((accum, item) => accum += item.amount, 0)
+    }
+
+    const getTotalPrice = () => {
+        return cart.reduce((accum, item) => accum += item.amount * item.price)
+    }    
+
+    console.log(cart)
+
+    return(
+        <CartContext.Provider value={{ cart, addItem, removeItem, emptyCart, getItemQuantity, getTotalPrice }}>
+            {props.children}
+        </CartContext.Provider>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.jsx';
 
+import { CartProvider } from './context/CartContext.js';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <CartProvider>
     <App />
-  </React.StrictMode>
+  </CartProvider>
 );
